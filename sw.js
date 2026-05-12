6c8058d00e8a666f88a4f292e7be5e5201bec3af
reinhardloher-dotcom <reinhard.loher@gmail.com>
2026-05-12 06:30:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pokeGrader-v11';
+const CACHE = 'pokeGrader-v12';
 const ASSETS = ['./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', e => {
